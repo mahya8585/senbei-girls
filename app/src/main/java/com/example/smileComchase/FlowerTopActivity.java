d3805cc3a41343c39a99a0fd1817e1668f855fc3
maaya-ubuntu <huki.j37@gmail.com>
2021-04-15 11:49:13 +0900

    カメラ機能追加
    きさきさん作成処理マージ

--- app/src/main/java/com/example/smileComchase/FlowerTopActivity.java
+++ app/src/main/java/com/example/smileComchase/FlowerTopActivity.java
@@ -3,25 +3,67 @@ package com.example.smileComchase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class FlowerTopActivity extends AppCompatActivity {
+
+    private final static int RESULT_CAMERA = 1001;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flower_top);
-        Button sendbutton = findViewById(R.id.button);
-        sendbutton.setOnClickListener(new View.OnClickListener() {
+        imageView = findViewById(R.id.image_view);
+        Button registeredblog = findViewById(R.id.submitbutton);
+        registeredblog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplication(), DiaryActivity.class);
-                startActivity(intent);
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE );
+                startActivityForResult(intent, RESULT_CAMERA);
             }
-        }
-        );
+        });
+
+        Button camerabutton = findViewById(R.id.opencamera);
+        camerabutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE );
+                startActivityForResult(intent, RESULT_CAMERA);
+            }
+        });
+
 
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // requestしたコードがRESULT_CAMERAであるか確認する
+        Bitmap bitmap;
+        // cancelしたケースも含む
+        if (data.getExtras() == null) {
+            Log.d("debug", "cancel ?");
+            return;
+        } else {
+            bitmap = (Bitmap) data.getExtras().get("data");
+            if (bitmap != null) {
+                // 画像サイズを計測
+                int bmpWidth = bitmap.getWidth();
+                int bmpHeight = bitmap.getHeight();
+                Log.d("debug", String.format("w= %d", bmpWidth));
+                Log.d("debug", String.format("h= %d", bmpHeight));
+            }
+        }
+
+
+
+        imageView.setImageBitmap(bitmap);
+    }
 }
+
---
+++ app/src/main/java/com/example/smileComchase/registerdblog.java
@@ -0,0 +1,14 @@
+package com.example.smileComchase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registerdblog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registerdblog);
+    }
+}
